25573f8013f062c4408de493ddeef71cae6f734b
Antonio Gabriel <antognani1@gmail.com>
2026-06-28 02:50:28 +0000

    vite fixing

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/NoSQLExplorer/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
